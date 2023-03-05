97db891db8713e612b6906a434b504492c9617ee
Valery Zakharov <zakharov.vp@gmail.com>
2023-03-05 11:42:05 +0300

    04.03. Developing our own middleware

---
+++ cmd/web/middleware.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/justinas/nosurf"
+	"net/http"
+)
+
+func NoServe(next http.Handler) http.Handler {
+	csrfHandler := nosurf.New(next)
+
+	csrfHandler.SetBaseCookie(http.Cookie{
+		HttpOnly: true,
+		Path:     "/",
+		Secure:   false,
+		SameSite: http.SameSiteLaxMode,
+	})
+
+	return csrfHandler
+}
--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/zahnah/study-app
 go 1.19
 
 require github.com/go-chi/chi/v5 v5.0.8
+
+require github.com/justinas/nosurf v1.1.1 // indirect
