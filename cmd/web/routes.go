6fd911a2ab490b2d6e4ee251fa18edf7a3a55c37
Valery Zakharov <zakharov.vp@gmail.com>
2023-03-05 11:20:19 +0300

    04.02 The chi router on GitHub

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,17 +1,20 @@
 package main
 
 import (
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/zahnah/study-app/pkg/config"
 	"github.com/zahnah/study-app/pkg/handlers"
 	"net/http"
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	mux := pat.New()
+	mux := chi.NewRouter()
 
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	mux.Use(middleware.Recoverer)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 
 	return mux
 }
